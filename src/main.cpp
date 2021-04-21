f76853885c22061f2ce2d3a50316990bce3c0e13
Zebio <mateuseusebio@gmail.com>
2021-04-21 20:27:29 -0300

    impressao do valor via serial a comentarios

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,15 @@
 #define pino_de_medicao 33
 
 
+
 /*---------------------Constantes de Projeto ----------------------*/
-const char * ssid = "nome_da_rede";
-const char * password = "nome_da_senha";
+#define valorAdc         4095
+#define tensaoMaxima     16.5
+
+
+
+const char * ssid = "tira_o_zoio";
+const char * password = "jabuticaba";
 
 
 /*---------------------Variáveis Globais    ----------------------*/
@@ -30,11 +36,23 @@ void setup() {
   Serial.println("WiFi conectada");      //
   Serial.println("Endereço de IP: ");    //
   Serial.println(WiFi.localIP());        //mostra o endereço IP no monitor
+
 }
 
 void loop() {
   tensao = analogRead(pino_de_medicao);
-  Serial.println(tensao);
+  Serial.print("Valor DAC: ");
+  Serial.print(tensao);
+  Serial.print("   Valor em Tensão");
+  tensao = (tensao/valorAdc*tensaoMaxima); //como o valor medido de tensão é atenuado em 5 vezes,
+                               // 16.5V na ponta de prova equivale a 3.3V no ESP.
+                               // Para voltar ao valor original dividimos por
+                               // 4095 (3.3V no ADC) multiplicado por 16.5V
+   Serial.println(tensao);
+   tensao += (tensao*0.1);
+   Serial.print("   Valor Corrigido");
+   Serial.println(tensao);
+  
   delay(1000);
 
 ///4095)*16.5);
