8e21ccf75d6491e9f05086a67cc949397e13e6d8
Zebio <mateuseusebio@gmail.com>
2021-05-09 23:39:12 -0300

    Inclusao da página web no código, cálculo da porcentagem de bateria, conexão wifi e servidor http

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <fstream>
+#include <iostream>
+
 
 /*---------------------Mapeamento de Hardware----------------------*/
 #define pino_de_medicao 33
-
-
 
 /*---------------------Constantes de Projeto ----------------------*/
 #define valorAdc         4095
@@ -18,6 +19,35 @@ const char * password = "jabuticaba";
 
 /*---------------------Variáveis Globais    ----------------------*/
 float tensao =0.0;
+int dac12bits;
+int porcentagem;
+
+String header;
+
+
+
+
+
+
+/*---------------------Constantes Cstring para Armazenar os códigos HTML ----------------------*/
+const char *index1_html= "<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"icon\" href=\"data:,\"><title>Projeto07 - Nivel de Carga de bateria</title><script type = \"text/JavaScript\">       function AutoRefresh( t ) {   setTimeout(\"location.reload(true);\", t);}</script></head><style>html{color: #ffffff;font-family: Verdana;text-align: center;background-color: #0c0c27fd}h2{font-size: 30px;}p{font-size: 22px;}</style><body onload = \"JavaScript:AutoRefresh(1000);\"><h2>Monitoramento de Bateria IOT</h2><p> Tensao lida na bateria: ";
+const char *index2_html= "</p><p> Porcentagem estimada: ";
+const char *index3_html= "</p></body></html>";
+
+
+/*---------------------Objetos-------------------------------------*/
+WiFiServer server(80);
+
+
+
+/*----------------------Declaração das Funções -------------------*/
+void atualiza_leitura();//realiza a leitura da tensão e calcula a carga estimada
+
+void imprime_serial();  //imprime os valores da tensão e da carga no monitor serial
+
+void serverWifi();      //hospeda o servidor http
+
+
 
 /*---------------------Setup Inicial        ----------------------*/
 void setup() {
@@ -39,22 +69,87 @@ void setup() {
 
 }
 
+
 void loop() {
-  tensao = analogRead(pino_de_medicao);
-  Serial.print("Valor DAC: ");
-  Serial.print(tensao);
-  Serial.print("   Valor em Tensão");
-  tensao = (tensao/valorAdc*tensaoMaxima); //como o valor medido de tensão é atenuado em 5 vezes,
+  while(WiFi.status() != WL_CONNECTED) //verifica se ainda está conectado wireless
+  {
+    WiFi.begin(ssid, password);        //tenta conectar denovo a cada 0.5 seg
+    Serial.print(".");                   
+    delay(500);                         
+  }
+
+ // atualiza_leitura();
+ // imprime_serial();
+  serverWifi();
+  //delay(1000);
+}
+
+/*--------------------------Código das funções ---------------------*/
+
+void atualiza_leitura()
+{
+  dac12bits = analogRead(pino_de_medicao);
+  tensao = (dac12bits/valorAdc*tensaoMaxima); //como o valor medido de tensão é atenuado em 5 vezes,
                                // 16.5V na ponta de prova equivale a 3.3V no ESP.
                                // Para voltar ao valor original dividimos por
                                // 4095 (3.3V no ADC) multiplicado por 16.5V
-   Serial.println(tensao);
-   tensao += (tensao*0.1);
-   Serial.print("   Valor Corrigido");
-   Serial.println(tensao);
-  
-  delay(1000);
+  porcentagem = (tensao - 3.60)/0.005;
+  if(porcentagem>100)
+    porcentagem=100;
+  if (porcentagem<0)
+    porcentagem=0;
+}
 
-///4095)*16.5);
-  // put your main code here, to run repeatedly:
+void imprime_serial()
+{
+  Serial.print("   Leitura DAC: ");
+  Serial.print(dac12bits);
+  Serial.print("   Tensão: ");
+  Serial.print(tensao);
+  Serial.print("   Carga Estimada: ");
+  Serial.print(porcentagem);
+  Serial.println("%");
+}
+
+
+void serverWifi()
+{
+  WiFiClient client = server.available();
+  Serial.println("teste"); 
+  if (client) {                            
+    Serial.println("Novo Cliente Conectado");        
+    String currentLine = "";               
+    while (client.connected()) {           
+      if (client.available()) {            
+        char c = client.read();            
+        Serial.write(c);                   
+        header += c;
+        if (c == '\n') {                 
+          if (currentLine.length() == 0||currentLine=="\n") {
+
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+
+          Serial.println("Imprimindo pagina HTML");
+
+          client.println(index1_html);
+       //   client.println(tensao);
+          client.println(index2_html);
+         // client.println(porcentagem);
+          client.println(index3_html);
+          client.println();
+          break;
+          }
+          else currentLine = ""; //senão, impede string de fizer com espaços em branco
+        }
+        else if (c != '\r')  
+          currentLine += c;      //adiciona caractere como parte da mensage
+      }
+    }
+    header = "";                              //limpa header
+    client.stop();                            //finaliza conexão
+    Serial.println("Cliente desconectado"); 
+    Serial.println("");  
+  }
 }
